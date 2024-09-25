1c66dfa7013c9c9164917d799294c72f0678e7f7
Andrew Loloy <druloloy@proton.me>
2024-09-25 19:58:30 +0000

    version bump

---
+++ index.ts
@@ -0,0 +1,7 @@
+import WordBank from './src/bank'
+import Yordle from './src/core'
+
+export {
+  WordBank,
+  Yordle
+}
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import WordBank from './bank'
-import Yordle from './core'
-
-export {
-  WordBank,
-  Yordle
-}
