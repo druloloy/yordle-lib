8e4b298216b3ad633163ffacb5408139ec07b692
Andrew Loloy <druloloy>
2024-10-05 16:59:42 +0800

    Added few properties to library

--- src/core/index.ts
+++ src/core/index.ts
@@ -7,109 +7,120 @@ import { IYordle, LetterCountType, ResultType } from "../types";
  * @implements {IYordle}
  */
 class Yordle implements IYordle {
-    letterCount: LetterCountType;
-    word: string;
-  
+    private letterCount: LetterCountType;
+    private word: string;
+    public entries: ResultType[] = [];
+    public availableLetters: Set<string> = new Set('abcdefghijklmnopqrstuvwxyz');
+    public unavailableLetters: Set<string> = new Set();
+
     /**
-     * Initialize the Yordle object with a word.
-     * @param {string} inputWord The word to be used as the Yordle word
+     * Create a new Yordle instance with the given 5-letter word.
+     * @param {string} input - the 5-letter word to be guessed.
+     * @throws {TypeError} If the word is empty, not 5 letters long, or not in the dictionary.
      */
     constructor(input: string) {
-      this.validateWord(input);
-      this.word = input.toLowerCase();
-      // Count the letters in the word
-      this.letterCount = this.countLetters(input);
+        this.validateWord(input);
+        this.word = input.toLowerCase();
+        this.letterCount = this.countLetters(this.word);
     }
-  
-    
+
     /**
-     * Validate the given word to ensure it is a 5-letter word.
-     * @param {string} word - The word to be validated
-     * @throws {TypeError} - If the word is empty or not 5 letters long
+     * Validates the given word against the following conditions:
+     *  - The word is not empty.
+     *  - The word is 5 letters long.
+     *  - The word is in the dictionary (WordBank).
+     * @param {string} word - The word to be validated.
+     * @throws {TypeError} If the word fails to satisfy any of the above conditions.
+     * @private
      */
     private validateWord(word: string) {
-      if (!word) {
-        throw new TypeError("Assign a 5-letter word to guess.");
-      }
-      if (word.length !== 5) {
-        throw new TypeError("Word must have 5 letters only.");
-      }
-      if (!WordBank.verify(word)) {
-        throw new TypeError("Word doesn't exist in the dictionary.");
-      }
+        if (!word || word.length !== 5 || !WordBank.verify(word)) {
+            throw new TypeError("Invalid word. Must be a 5-letter word from the dictionary.");
+        }
     }
-  
+
     /**
-     * Compares the given guess word with the Yordle word.
-     * @param {string} inputWord The guess word
-     * @returns {ResultType} An array of objects with letter as key and
-     * the value will be either 'exact', 'exists' or 'wrong' depending on the
-     * position and existence of the letter in the Yordle word.
+     * Make a guess with the given 5-letter word.
+     * @param {string} inputWord - The 5-letter word to be guessed.
+     * @returns {ResultType} An array of objects, each representing the result of a letter match.
+     *   - "exact" if the letter is in the correct position,
+     *   - "exists" if the letter exists in the word but not in the correct position,
+     *   - "wrong" if the letter does not exist in the word.
+     * @throws {TypeError} If the word is empty, not 5 letters long, or not in the dictionary.
      */
     guess(inputWord: string): ResultType {
-      this.validateWord(inputWord);
-      const guessWord = inputWord.toLowerCase();
-  
-      // Count the letters that exist in the word
-      const existCounter: LetterCountType = {};
-      // Count the letters that are in the exact position
-      const exactCounter: LetterCountType = {};
-      // The results of the guess
-      let results = guessWord.split('').map((letter, index) => {
-        const correctLetter: string = this.word[index];
-  
-        if (letter === correctLetter) {
-          exactCounter[letter] = (exactCounter[letter] || 0) + 1;
-          return { [letter]: "exact" };
-        } else if (this.word.includes(letter)) {
-          existCounter[letter] = (existCounter[letter] || 0) + 1;
-          return { [letter]: "exists" };
-        } else {
-          return { [letter]: "wrong" };
+        this.validateWord(inputWord);
+        const guessWord = inputWord.toLowerCase();
+        const result: ResultType = new Array(5);
+        const remainingLetters = { ...this.letterCount };
+
+        // First pass: Mark exact matches
+        for (let i = 0; i < 5; i++) {
+            const guessLetter = guessWord[i];
+            if (guessLetter === this.word[i]) {
+                result[i] = { [guessLetter]: "exact" };
+                remainingLetters[guessLetter]--;
+            }
         }
-      });
-  
-      // Recheck the results and correct the existing letters based on the exact count
-      results = this.recheck(results as ResultType, exactCounter)
-  
-      return results as ResultType;
-    }
-  
-    
-    /**
-     * Rechecks the results of the guess and corrects the existing letters
-     * based on the exact count.
-     * @param {ResultType} results - The results of the guess
-     * @param {LetterCountType} exact - The count of letters that are in the exact position
-     * @returns {ResultType} - The corrected results
-     */
-    private recheck(results: ResultType, exact: LetterCountType): ResultType {
-      return results.map((result) => {
-        const letter = Object.keys(result)[0];
-  
-        // If the letter is marked as existing and the exact count of the letter
-        // is equal to the total count of the letter, then the letter is not
-        // in the correct position, so mark it as wrong
-        if (result[letter] === 'exists' && exact[letter] === this.letterCount[letter]) {
-          result[letter] = 'wrong';
+
+        // Second pass: Mark exists or wrong
+        for (let i = 0; i < 5; i++) {
+            if (result[i]) continue; // Skip already marked positions
+
+            const guessLetter = guessWord[i];
+            if (remainingLetters[guessLetter] > 0) {
+                result[i] = { [guessLetter]: "exists" };
+                remainingLetters[guessLetter]--;
+            } else {
+                result[i] = { [guessLetter]: "wrong" };
+                this.removeAvailableLetter(guessLetter);
+            }
         }
+
+        this.entries.push(result);
         return result;
-      });
     }
-  
-    
+
     /**
-     * Count the letters in a word
-     * @param {string} word - The word to count the letters in
-     * @returns {LetterCountType} - An object with the letter as the key and the count as the value
+     * Count the occurrences of each letter in the given word.
+     * @param {string} word - The word to count letters for.
+     * @returns {LetterCountType} An object with each letter as a key and its count as the value.
+     * @private
      */
     private countLetters(word: string): LetterCountType {
-      return word.split('').reduce((acc, letter) => {
-        // If the letter is already in the object, increment the count, otherwise add it with a count of 1
-        acc[letter] = (acc[letter] || 0) + 1;
-        return acc;
-      }, {} as LetterCountType);
+        return word.split('').reduce((acc, letter) => {
+            acc[letter] = (acc[letter] || 0) + 1;
+            return acc;
+        }, {} as LetterCountType);
     }
-  }
-  
-  export default Yordle
+
+    /**
+     * Remove a letter from the set of available letters and add it to the set of unavailable letters.
+     * @param {string} letter - The letter to remove from the set of available letters.
+     * @private
+     */
+    private removeAvailableLetter(letter: string) {
+        if (this.availableLetters.has(letter)) {
+            this.availableLetters.delete(letter);
+            this.unavailableLetters.add(letter);
+        }
+    }
+
+    public loadEntries(entries: ResultType[]) {
+        this.entries = entries;
+
+        // Remove unavailable letters from the set of available letters
+        for (const entry of entries) {
+          for (const item of entry) {
+            const [key, value] = Object.entries(item)[0];
+            if (value === 'wrong') {
+              this.removeAvailableLetter(key);
+            }
+          }
+        }
+
+        console.log(this.unavailableLetters);
+    }
+}
+
+export default Yordle;
--- tsup.config.js
+++ tsup.config.js
@@ -4,6 +4,7 @@ export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs", "esm"], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
+  bundle: true, // disclude empty folders from being bundled
   splitting: false,
   sourcemap: true,
   clean: true,
